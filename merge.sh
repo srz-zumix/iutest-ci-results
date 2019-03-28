648927312af66e789e17aeb56d53ddfdebcaa576
srz_zumix <zumix.cpp@gmail.com>
2019-03-28 10:32:56 +0900

    update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-RETRY=$1
-TARGET_BRANCH=$2
+TARGET_BRANCH=$1
+RETRY=${2:-5}
+
+if [ -z ${TARGET_BRANCH} ]; then
+  echo "./merge.sh <target_branch> [retry]"
+  exit 1
+fi
+
+echo ${TARGET_BRANCH}:${RETRY}
 
 for i in `seq 0 ${RETRY}`; do
   echo "i = $i"
-  try_merge.sh ${TARGET_BRANCH}
+  ./try_merge.sh ${TARGET_BRANCH}
   if [ $? -eq 0 ]; then
     exit 0
   fi
--- try_merge.sh
+++ try_merge.sh
@@ -3,8 +3,8 @@
 set -e
 
 git status
-git branch -D all
+git branch -D all || true
 git fetch -p --all
 git checkout -b all origin/all
 git merge origin/$1
-git push
+git push -n
