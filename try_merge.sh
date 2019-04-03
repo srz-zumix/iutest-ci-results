d7bd7f9d14047c9aabe45b2244d36126cbdcfbf7
srz_zumix <zumix.cpp@gmail.com>
2019-04-03 20:58:16 +0900

    Update try_merge.sh

--- try_merge.sh
+++ try_merge.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 git status
 git branch -D all 2>/dev/null || true
-echo "============ fetch ============"
 git fetch -p --all
-echo "============   co  ============"
 git checkout -b all origin/all
-echo "============ merge ============"
 #git merge -s theirs origin/$1
 git merge -Xtheirs origin/$1
-echo "============ push  ============"
 git push ${DRYRUN}
